e2fd9bf19c520e5d149c96ddb44a866b1394521a
Graziano Casto <graziano.casto@outlook.com>
2023-09-19 21:33:39 +0200

    *finish parametization of components and clean repo code

---
+++ photographer-portfolio.ts
@@ -0,0 +1,104 @@
+const configurations = {
+  site: {
+    title: "Graziano Casto · Street Photograph",
+    description: "Graziano Casto · Street Photograph",
+  },
+  header: {
+    title: "Graziano Casto",
+    desciption: "Street Photography & Reportage",
+    caption:
+      "I’m an Italian lad who has ingeniously fused his two greatest passions: photography and travel, weaving captivating tales of the places and people he encounters through his evocative imagery.",
+    items: [
+      {
+        reference: "01",
+        name: "Home",
+        link: "/home",
+      },
+      {
+        reference: "02",
+        name: "About",
+        link: "/about",
+      },
+      {
+        reference: "03",
+        name: "Galleries",
+        link: "/galleries",
+      },
+      {
+        reference: "03",
+        name: "Contact",
+        link: "/contact",
+      },
+      {
+        reference: "05",
+        name: "Journal",
+        link: "/journal",
+      },
+    ],
+  },
+  homepage: {
+    title: "Capturing the essence of everyday life, one street at a time.",
+    subtitle: "Exploring the world through the lens of the everyday.",
+    gallery: [
+      {
+        source: "https://drive.google.com/uc?id=1Vx5Zha1vjx7d6ar-FSarZux8d882slx9",
+        title: "Hero picture",
+      },
+      {
+        source: "https://drive.google.com/uc?id=1Vx5Zha1vjx7d6ar-FSarZux8d882slx9",
+        title: "Hero picture",
+      },
+      {
+        source: "https://drive.google.com/uc?id=1cOeAWSd5ceZUnpAqscIhY8q7l0CDvAYy",
+        title: "Hero picture",
+      },
+      {
+        source: "https://drive.google.com/uc?id=1Vx5Zha1vjx7d6ar-FSarZux8d882slx9",
+        title: "Hero picture",
+      },
+      {
+        source: "https://drive.google.com/uc?id=1cOeAWSd5ceZUnpAqscIhY8q7l0CDvAYy",
+        title: "Hero picture",
+      },
+    ],
+  },
+  footer: {
+    title: "Graziano Casto",
+    caption: "Capturing the essence of everyday life, one street at a time.",
+    copyright: "© 2023 Graziano Casto",
+    items: [
+      {
+        title: "Social",
+        links: [
+          {
+            name: "Instagram",
+            link: "https://instagram.com/",
+            isExternal: true,
+          }
+        ],
+      },
+      {
+        title: "Sitemap",
+        links: [
+          {
+            name: "",
+            link: "",
+            isExternal: false,
+          }
+        ],
+      },
+      {
+        title: "Legal",
+        links: [
+          {
+            name: "",
+            link: "",
+            isExternal: false,
+          }
+        ],
+      },
+    ],
+  },
+};
+
+export default configurations;
