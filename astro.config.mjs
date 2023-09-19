1054d34dcb74a56224cec3b93ca3cca4ca0f82c5
Graziano Casto <graziano.casto@outlook.com>
2023-09-19 22:26:17 +0200

    *added gitignore and vercel suppport

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [tailwind(), compress()],
+  output: "server",
+  adapter: vercel()
 });
