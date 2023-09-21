429c3729289735246165dabe41df1954af599230
Graziano Casto <graziano.casto@outlook.com>
2023-09-21 20:44:30 +0200

    *added doc

--- README.md
+++ README.md
@@ -1,7 +1,187 @@
 # Photographer Portfolio
 
-## Whether you're a developer or just a photographer, showcase your portfolio online with just a few clicks, without the need to write any code, using the photographer-portfolio template.
-
 ![cover](https://github.com/graz-dev/photograper-portfolio/assets/62215881/2c597a50-7e17-4a63-b579-8610e2fc1071)
 
- ![Vercel](https://vercelbadge.vercel.app/api/[graz-dev]/[photograper-portfolio])
+### [Live Demo]()
+
+In today's world, having a website is like carving out a piece of the digital realm just for yourself. This is especially crucial for creative professionals who need their own space to showcase their work. Unfortunately, if you lack technical know-how, it can often be too complicated for the average person to publish their work somewhere other than social media, creating their own portfolio.
+
+That's where this project comes in: to empower everyone, whether they have technical skills or not, to effortlessly create their own photography portfolio. All it takes is a simple configuration file edit, and you can release it online for free. Your creative corner of the web awaits!
+
+## Features
+
+This project enables anyone to create a photography portfolio with the following features:
+
+1. [X] Homepage with a Masonry Gallery component to showcase your photos
+2. [X] Lightbox feature for the Masonry Gallery component
+3. [X] Preventing unauthorized file downloads to safeguard your photos from being reused without consent.
+4. [X] About and Contact Pages with Email Subscription Capability
+5. [X] Entire website fully configurable via a single configuration file - no need to change your code!
+6. [X] Easily deployable on [Vercel](https://vercel.com/)
+
+Numerous features are in the pipeline for development in the upcoming period, including:
+
+1. [ ] Blog section with the possibility to post markdown posts
+2. [ ] Possibility to use a CDN to serve your images
+3. [ ] Possibility to extract shoot information from image files to compile your statistics as photographer (eg: most used focal lense)
+4. [ ] Possibility to organize your photos into galleries
+
+I welcome any ideas or suggestions! Feel free to reach out to us here:
+
+- [Instagram](https://www.instagram.com/graz.ph)
+- [Email](mailto:cst.grzn@gmail.com)
+
+Support the project by leaving a star here on GitHub and following me on my social media profiles.
+
+## Configurations
+
+```json
+
+site: {
+    title: "Photographer · Portfolio",
+    description: "Photographer · Portfolio",
+  },
+  header: {
+    title: "Photographer",
+    desciption: "Portfolio",
+    caption: "Caption",
+    items: [
+      {
+        reference: "01",
+        name: "Home",
+        link: "/",
+      },
+      {
+        reference: "02",
+        name: "About",
+        link: "/about",
+      },
+      {
+        reference: "03",
+        name: "Contact",
+        link: "/contact",
+      },
+    ],
+  },
+  homepage: {
+    title: "Homepage Title",
+    subtitle: "Homepage Subtitle",
+    gallery: [
+      {
+        source: "/gallery/placeholder-1.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-2.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-1.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-2.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-1.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-1.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-2.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-1.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-2.png",
+        title: "",
+      },
+      {
+        source: "/gallery/placeholder-1.png",
+        title: "",
+      },
+    ],
+  },
+  footer: {
+    title: "Photographer",
+    caption: "Caption",
+    copyright: "© 2023 Photographer",
+    items: [
+      {
+        title: "Social",
+        links: [
+          {
+            name: "Instagram",
+            link: "",
+            isExternal: true,
+          },
+        ],
+      },
+      {
+        title: "Sitemap",
+        links: [
+          {
+            name: "",
+            link: "",
+            isExternal: false,
+          },
+        ],
+      },
+      {
+        title: "Legal",
+        links: [
+          {
+            name: "",
+            link: "",
+            isExternal: false,
+          },
+        ],
+      },
+    ],
+  },
+  about: {
+    title: "About me",
+    image: {
+      source: "/gallery/placeholder-1.png",
+      title: "About Me",
+    },
+    paragraphs: [
+      {
+        ref: 1,
+        content:
+          "About me content",
+      },
+    ]
+  },
+  contacts: {
+    rowCards: {
+      enabled: false,
+      items: [
+        {
+          title: "",
+          svgIcon: "",
+          paragraphs: [
+            {
+              ref: 1,
+              content: "",
+            },
+          ],
+        },
+      ],
+    },
+    contactForm: {
+      title: "Contact me",
+      subtitle: "Let's keep in touch",
+      action: "",
+    },
+  },
+}
+
+```
--- photographer-portfolio.ts
+++ photographer-portfolio.ts
@@ -1,13 +1,12 @@
 const configurations = {
   site: {
-    title: "Graziano Casto · Street Photograph",
-    description: "Graziano Casto · Street Photograph",
+    title: "Photographer · Portfolio",
+    description: "Photographer · Portfolio",
   },
   header: {
-    title: "Graziano Casto",
-    desciption: "Street Photography & Reportage",
-    caption:
-      "I’m an Italian lad who has fused his two greatest passions: photography and travel, weaving captivating tales of the places and people he encounters through his evocative imagery.",
+    title: "Photographer",
+    desciption: "Portfolio",
+    caption: "Caption",
     items: [
       {
         reference: "01",
@@ -19,108 +18,70 @@ const configurations = {
         name: "About",
         link: "/about",
       },
-      /*{
-        reference: "03",
-        name: "Galleries",
-        link: "/galleries",
-      },*/
       {
         reference: "03",
         name: "Contact",
         link: "/contact",
       },
-      /*{
-        reference: "05",
-        name: "Journal",
-        link: "/journal",
-      },*/
     ],
   },
   homepage: {
-    title: "Capturing the essence of everyday life, one street at a time.",
-    subtitle: "Exploring the world through the lens of the everyday.",
+    title: "Homepage Title",
+    subtitle: "Homepage Subtitle",
     gallery: [
       {
-        source: "/gallery/travel-1.webp",
+        source: "/gallery/placeholder-1.png",
         title: "",
       },
       {
-        source: "/gallery/travel-2.webp",
+        source: "/gallery/placeholder-2.png",
         title: "",
       },
       {
-        source: "/gallery/travel-3.webp",
+        source: "/gallery/placeholder-1.png",
         title: "",
       },
       {
-        source: "/gallery/travel-4.webp",
+        source: "/gallery/placeholder-2.png",
         title: "",
       },
       {
-        source: "/gallery/travel-5.webp",
+        source: "/gallery/placeholder-1.png",
         title: "",
       },
       {
-        source: "/gallery/travel-6.webp",
+        source: "/gallery/placeholder-1.png",
         title: "",
       },
       {
-        source: "/gallery/street-1.webp",
+        source: "/gallery/placeholder-2.png",
         title: "",
       },
       {
-        source: "/gallery/street-2.webp",
+        source: "/gallery/placeholder-1.png",
         title: "",
       },
       {
-        source: "/gallery/street-3.webp",
+        source: "/gallery/placeholder-2.png",
         title: "",
       },
       {
-        source: "/gallery/street-4.webp",
-        title: "",
-      },
-      {
-        source: "/gallery/street-5.webp",
-        title: "",
-      },
-      {
-        source: "/gallery/street-6.webp",
-        title: "",
-      },
-      {
-        source: "/gallery/street-7.webp",
-        title: "",
-      },
-      {
-        source: "/gallery/street-8.webp",
-        title: "",
-      },
-      {
-        source: "/gallery/street-9.webp",
-        title: "",
-      },
-      {
-        source: "/gallery/street-10.webp",
-        title: "",
-      },
-      {
-        source: "/gallery/street-11.webp",
+        source: "/gallery/placeholder-1.png",
         title: "",
       },
     ],
   },
   footer: {
-    title: "Graziano Casto",
-    caption: "Capturing the essence of everyday life, one street at a time.",
-    copyright: "© 2023 Graziano Casto",
+    title: "Photographer",
+    caption: "Caption",
+    copyright: "© 2023 Photographer",
     items: [
       {
         title: "Social",
         links: [
           {
             name: "Instagram",
-            link: "https://instagram.com/",
+            link: "",
             isExternal: true,
           },
         ],
@@ -150,40 +111,16 @@ const configurations = {
   about: {
     title: "About me",
     image: {
-      source: "/assets/about-me.jpg",
+      source: "/gallery/placeholder-1.png",
       title: "About Me",
     },
     paragraphs: [
       {
         ref: 1,
-        content: "I'm an Italian photographer driven by an unquenchable curiosity and a profound love for globetrotting. My ultimate purpose is to capture the exquisite beauty of our world and convey the emotions it stirs within me. I refuse to confine myself to a specific photographic genre because storytelling is at the core of my artistic journey. On my platform, you'll find articles in the \"Journal\" section that provide insights into my creative process and narratives, as well as galleries to immerse yourself in. What truly ignites my creative passion is black and white photography. It allows me to peel away distractions, focusing solely on subjects and utilizing light as my exclusive medium of expression. In this realm, color takes a back seat, and I prefer to communicate through the raw interplay of light and shadow, using contrasts to paint the stories within my frames. It's a journey back to the essence, a way to spotlight what truly matters in each image.",
+        content:
+          "About me content",
       },
-    ],
-    statistics: {
-      enabled: false,
-      title: "",
-      subtitle: "",
-      items: [
-        /*{
-          ref: 1,
-          name: "",
-          value: "",
-        },*/
-      ],
-    },
-    columnCards: {
-      enabled: false,
-      title: "",
-      columns: [
-        [
-          /*{
-            reference: "01",
-            title: "",
-            subtitle: "",
-          },*/
-        ],
-      ],
-    },
+    ]
   },
   contacts: {
     rowCards: {
@@ -195,18 +132,18 @@ const configurations = {
           paragraphs: [
             {
               ref: 1,
-              content: ""
-            }
-          ]
-        }
-      ]
+              content: "",
+            },
+          ],
+        },
+      ],
     },
     contactForm: {
       title: "Contact me",
       subtitle: "Let's keep in touch",
-      action: "https://formspree.io/f/mleyplqo"
-    }
-  }
+      action: "",
+    },
+  },
 };
 
 export default configurations;
